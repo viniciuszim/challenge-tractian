e1fdd80932202997080c5e4a96d8403fbeb6d698
Vinicius Miguel <viniciuszim@hotmail.com>
2024-12-13 15:45:27 -0300

    added responsive layout

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -7,7 +7,7 @@ interface BreadcrumbsProps {
 }
 const Breadcrumbs = ({ title, subTitle, children }: BreadcrumbsProps) => {
   return (
-    <div className="w-full h-[32px] flex justify-between">
+    <div className="w-full h-auto sm:h-[32px] flex justify-between flex-col sm:flex-row gap-2.5 sm:gap-0">
       <div className="gap-[7px] flex items-center">
         <h1 className="typography-heading-xl text-left">{title}</h1>
         <p className="typography-body-sm text-center">/ {subTitle}</p>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,22 +4,22 @@ import { faCubes as unitIcon } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
-    <header className="w-full h-12 px-4 flex items-center justify-between bg-primary text-white">
+    <header className="w-full sm:h-12 h-auto bg-primary text-white flex items-center justify-between flex-col sm:flex-row px-4 py-4 gap-2.5">
       {/* Logo */}
       <div className="text-xl font-bold">TRACTIAN</div>
 
       {/* Right Container */}
-      <div className="flex items-center gap-2.5 w-auto h-6">
-        <button className="button-active">
+      <div className="flex items-center gap-2.5 w-full sm:w-auto space-x-2 sm:space-x-4 sm:flex-row flex-col">
+        <button className="button-active w-full sm:w-auto">
           <FontAwesomeIcon icon={unitIcon} className="h-[14px] w-[14px] mr-2" />{" "}
           Apex Unit
         </button>
 
-        <button className="button-inactive">
+        <button className="button-inactive w-full sm:w-auto">
           <FontAwesomeIcon icon={unitIcon} className="h-[14px] w-[14px] mr-2" />{" "}
           Tobias Unit
         </button>
-        <button className="button-inactive">
+        <button className="button-inactive w-full sm:w-auto">
           <FontAwesomeIcon icon={unitIcon} className="h-[14px] w-[14px] mr-2" />{" "}
           Jaguar Unit
         </button>
