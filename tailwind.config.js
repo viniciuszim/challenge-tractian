96e67ee6059d6ec788053cb8e0470bef38ad7fb3
Vinicius Miguel <viniciuszim@hotmail.com>
2024-12-13 13:47:05 -0300

    added tailwind and fontawesome libs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#17192D",
+        blue: {
+          500: "#2188FF",
+          900: "#023B78",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
